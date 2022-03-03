174db23acba03d308e3add7ae6d5288d5ebffb3b
Francis <francisklinck@gmail.com>
2022-03-03 22:54:37 +0100

    remove serialize method

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, Float, CHAR
-from sqlalchemy.schema import Index
+
 from .database import Base
 
 
@@ -7,17 +7,7 @@ class Detection(Base):
     __tablename__ = 'detection'
     id = Column(Integer, primary_key=True, index=True)
     detection_time = Column(Integer, nullable=False)
-    mac = Column(CHAR(6*2+5), nullable=False)
+    mac = Column(CHAR(6 * 2 + 5), nullable=False)
     rssi = Column(Integer, nullable=False)
     baton_uptime_ms = Column(Integer, nullable=False)
     battery_percentage = Column(Float, nullable=False)
-
-    def serialize(self):
-        return {
-            'id': self.id,
-            'mac': self.mac,
-            'rssi': self.rssi,
-            'battery': self.battery_percentage,
-            'uptime_ms': self.baton_uptime_ms,
-            'detection_timestamp': self.detection_time
-        }
