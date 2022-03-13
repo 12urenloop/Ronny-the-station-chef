d46f0deb1aead3a270bbeec1499ae2591bc6e0b4
mcbloch <github@mcbloch.dev>
2022-03-13 20:38:06 +0100

    Save as timestamp type and return unixtimestamp

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, Float, CHAR
-from sqlalchemy.types import DateTime
+from sqlalchemy.types import DateTime, TIMESTAMP
 
 from .database import Base
 
@@ -7,7 +7,7 @@ from .database import Base
 class Detection(Base):
     __tablename__ = "detection"
     id = Column(Integer, primary_key=True, index=True)
-    detection_time = Column(DateTime, nullable=False)
+    detection_time = Column(TIMESTAMP, nullable=False)
     mac = Column(CHAR(6 * 2 + 5), nullable=False)
     rssi = Column(Integer, nullable=False)
     baton_uptime_ms = Column(Integer, nullable=False)
--- database/schemas.py
+++ database/schemas.py
@@ -1,7 +1,7 @@
 from typing import List
-from datetime import datetime
+from datetime import datetime, timezone
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class Detection(BaseModel):
@@ -10,11 +10,18 @@ class Detection(BaseModel):
     rssi: int
     baton_uptime_ms: int = Field(alias="uptime_ms")
     battery_percentage: float = Field(alias="battery")
-    detection_time: datetime = Field(alias="detection_timestamp")
+    detection_time: float = Field(alias="detection_timestamp")
+
+    @validator('*', pre=True)
+    def convert_time(cls, v) -> float:
+        if isinstance(v, datetime):
+            return v.timestamp()
+        return v
 
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
+        # json_encoders = {datetime: lambda v: v.timestamp()}
 
 
 class DetectionsResponse(BaseModel):
