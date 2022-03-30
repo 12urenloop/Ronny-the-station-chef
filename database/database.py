6dd6971f86194a81f0695fe0330aca0d7924e993
mcbloch <github@mcbloch.dev>
2022-03-30 14:32:19 +0200

    Make database url configurable via env

--- database/database.py
+++ database/database.py
@@ -3,8 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
+
 # DATABASE_URL = "sqlite:///./ronny.db"
-DATABASE_URL = "postgresql://ronny:ronnydbpassword@localhost/ronny"
+
+database_host = os.getenv("DATABASE_HOST", "localhost")
+database_port = os.getenv("DATABASE_PORT", "5432")
+database_user = os.getenv("DATABASE_USER", "ronny")
+database_password = os.getenv("DATABASE_PASSWORD", "ronnydbpassword")
+database_db = os.getenv("DATABASE_DB", "ronny")
+
+DATABASE_URL = f"postgresql://{database_user}:{database_password}@{database_host}:{database_port}/{database_db}"
 
 extra_args = {'connect_args': {"check_same_thread": False}} if DATABASE_URL.startswith('sqlite') else {}
 
